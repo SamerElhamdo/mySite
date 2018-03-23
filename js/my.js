173ce8b0f1e0d3b9a64f96d05b7f2ed74a8be128
Unknown <samer.elhamdoo@gmail.com>
2018-03-23 18:58:56 +0300

    تعديلات على المشروع
    html and css and js

---
+++ js/my.js
@@ -0,0 +1,80 @@
+$( document ).ready(function() {
+
+
+
+// statrt secrol------------------------------------------------------------//
+
+
+
+   var lastScrollTop = 0;
+	$( window ).scroll(function() {
+		var Scroll = $(window).scrollTop(),
+			calcblur =  Scroll/100,
+			blur = "blur(" + calcblur + "px)";
+		$(".heda_blur").css("filter",blur);
+		if (Scroll > lastScrollTop) {
+			$(".navbar").removeClass("scroll");
+		}
+		else {
+			if (Scroll > 1) {
+				$(".navbar").addClass("scroll");
+			}
+			else {
+				$(".navbar").removeClass("scroll");
+			}
+			
+		}
+		lastScrollTop = Scroll;
+		if ($(window).scrollTop() > 150) {
+			$(".scroll-hide").fadeOut();
+		}
+		else if ($(window).scrollTop() < 150) {
+			$(".scroll-hide").fadeIn();	
+		}
+	});
+
+
+
+// end secrol--------------------------------------------------------------------//
+
+
+
+
+
+
+
+// start typed--------------------------------------------------------------------//
+
+
+
+
+	var typed = new Typed('#typed', {
+	    strings: ["esign","evelopment"],
+	    typeSpeed: 120,
+	    backSpeed: 100,
+	    startDelay: 400,
+	    backDelay: 2500,
+	    smartBackspace: false,	
+	    fadeOut: false,
+	    showCursor: true,
+	    loop:true,
+	  });
+
+
+
+
+
+// start typed--------------------------------------------------------------------//
+
+
+// start navbar menu ------------------------------------------------------------//
+
+	$(".hedar ul li").click(function () {
+		$(this).addClass("active").siblings().removeClass("active");
+		var he = $(".hedar").height();
+		$("html").animate({scrollTop:he}, '1000');
+	});
+
+});
+
+
